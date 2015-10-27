3308a4bcf483c665b63c762b566f332bf959e731
Hershel Nashman <hershel.nashman@gmail.com>
2015-10-27 12:05:51 -0400

    exercise 2  completed

---
+++ exercise2.rb
@@ -0,0 +1,13 @@
+
+# A 15% tip of 55$ should equal 8.25$
+puts 55 * 0.15
+
+# example of .to_s which converts an int to a string
+puts "lotto" + " " + 649.to_s
+
+# example of interpolation
+answer = 45628 * 7839
+puts "The answer to 45628 * 7839 is equal to #{answer} "
+
+#should return true
+puts (true && false) || (false && true) || !(false && false)
